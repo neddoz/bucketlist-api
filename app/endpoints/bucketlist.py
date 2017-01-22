5607edd08fffc418e4fe7783191a6011adde5169
kayeli dennis <kdenno25@gmail.com>
2017-01-22 10:44:57 +0300

    implemented the serach and query limit parameter for bucket lists retrieved

--- app/endpoints/bucketlist.py
+++ app/endpoints/bucketlist.py
@@ -2,7 +2,7 @@ from flask_restful import Resource, reqparse, abort
 from app.models import BucketList, BucketlistItems
 from app import db
 from app.auth import multiple_auth
-from flask import g
+from flask import g, request
 from flask import jsonify, json
 
 
@@ -13,17 +13,15 @@ class BucketListRepo(Resource):
     def post(self):
         #get the the bucketlist items from the post request
         parser = reqparse.RequestParser()
-        parser.add_argument('name', type=str, help='Name of bucket list',
+        parser.add_argument('name', type=str,
+                            help='Name of bucket list is Required',
                             required=True)
         args = parser.parse_args()
 
         bucketlist_name = args["name"]
 
-        #validation
-        if bucketlist_name is None:
-            abort(400, msg='A bucket list name is required')
         if BucketList.query.filter_by(name=bucketlist_name).first() is not None:
-             abort(400, msg='Sorry A similar bucket list with the same name Exists!')
+             abort(403, msg='Sorry A similar bucket list with the same name Exists!')
 
         #create a bucketlist instance with the post data
         bucketlist = BucketList(name=bucketlist_name, user_id=g.user.id,
@@ -35,9 +33,14 @@ class BucketListRepo(Resource):
         return "Bucket list created succefully with the ID: %s"% bucketlist.id, 201
 
     def get(self, id=None):
+        searchword = request.args.get('q', '') or None
+        limit = request.args.get('limit', '') or 20
+        kwargs = {"user_id": g.user.id}
+        if searchword:
+            kwargs.update({"name": searchword})
         all_bucket_lists = []
+        bucketlists_retrieved = BucketList.query.filter_by(**kwargs).limit(limit).all()
         if not id:
-            bucketlists_retrieved = BucketList.query.filter_by(user_id=g.user.id).all()
             items = []
             for bucket_list in bucketlists_retrieved:
                 #retrieve a list of items linked to each bucket list
@@ -57,13 +60,17 @@ class BucketListRepo(Resource):
                     "date_modified": str(bucket_list.date_modified) or None,
                     "created_by": g.user.username}
                 all_bucket_lists.append(bucket_list_and_items)
+            if len(all_bucket_lists) == 0 and not searchword:
+                return {"message":"No bucket lists yet!"}, 200
+            if searchword and len(all_bucket_lists) == 0:
+                return {"message":"No bucketlists found for the search!"}, 200
             return all_bucket_lists
 
         #otherwise return one bucket list for id 2given
-        bucketlist_retrieved = BucketList.query.filter_by(id=id).first()
+        bucketlist_retrieved = BucketList.query.filter_by(id=id, user_id=g.user.id).first()
 
         if not bucketlist_retrieved:
-            abort(400, msg='No bucket list with such an ID')
+            abort(404, msg='Not Found!')
 
         items = []
         retrieved_items = BucketlistItems.query.filter_by(bucketlist_id=bucketlist_retrieved.id).all()
@@ -81,14 +88,10 @@ class BucketListRepo(Resource):
                 "date_modified": str(bucketlist_retrieved.date_modified) or None,
                 "created_by": g.user.username}
 
-    def put(self, id=None):
-        #validation
-        if not id:
-            abort(400, msg="An id is manadatory to Ammend a Bucketlist!")
-
+    def put(self, id):
         bucketlist_retrieved = BucketList.query.filter_by(id=id).first()
         if not bucketlist_retrieved:
-            abort(400, msg="No record found!")
+            abort(404, msg="No record found!")
 
         parser = reqparse.RequestParser()
         parser.add_argument('name', type=str, help='Name of bucket list',
@@ -111,7 +114,7 @@ class BucketListRepo(Resource):
 
         bucketlist_retrieved = BucketList.query.filter_by(id=id).first()
         if not bucketlist_retrieved:
-            abort(400, msg="No record found!")
+            abort(404, msg="No record found!")
 
         #persist the changes in the database
         db.session.delete(bucketlist_retrieved)
