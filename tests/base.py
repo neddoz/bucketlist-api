d28069123c3d61319190db1184ddc449ae406bec
kayeli dennis <kdenno25@gmail.com>
2017-01-22 10:47:45 +0300

    Added additional routes for the tests

--- tests/base.py
+++ tests/base.py
@@ -1,8 +1,10 @@
+from flask import json
 from flask_testing import TestCase
 from app import create_app, db, api
 from app.models import User, BucketList
 from app.endpoints.user import Index, RegisterUser, LoginUser
 from app.endpoints.bucketlist import BucketListRepo
+from app.endpoints.bucketlist_items import BucketListItemsRepo
 
 class BaseTest(TestCase):
 
@@ -18,8 +20,22 @@ class BaseTest(TestCase):
         api.add_resource(Index, '/', '/api/v1', endpoint='index')
         api.add_resource(RegisterUser, '/auth/register', endpoint='register')
         api.add_resource(LoginUser, '/auth/login', endpoint='login')
+        api.add_resource(BucketListItemsRepo, '/bucketlists/<id>/items/',
+                                endpoint='bucketlist_items')
+        api.add_resource(BucketListItemsRepo, '/bucketlists/<id>/items/<item_id>')
+        api.add_resource(BucketListRepo, '/bucketlists/<id>',
+                                endpoint='bucketlists')
+        api.add_resource(BucketListRepo, '/bucketlists/')
         # api.add_resource(BucketListRepo, 'api/v1/bucketlists/', endpoint='bucketlist')
 
+        #register a user
+        self.client.post('/api/v1/auth/register', data=self.user_credentials)
+
+        self.response = self.client.post('/api/v1/auth/login', data=self.user_credentials)
+        self.response_data_in_json_format = json.loads(self.response.data.decode('utf-8'))
+
+        self.token = 'Token '+self.response_data_in_json_format['token']
+        self.headers = {'Authorization': self.token}
     def tearDown(self):
         db.session.remove()
         db.drop_all()
